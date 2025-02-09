5fdad89c6714bcb352b5c4a288dc6d6598baa799
abdullahaksu <175130227+abdullahaksu@users.noreply.github.com>
2025-02-09 22:32:47 +0300

    v0.01 - Changed Somethings

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+   int number = 5;
+   float sayi = 5.5f;
+
+   string isim = "abdullah";
+
+   bool dogruMu=true;
+
+   void testFunction()
+   {
+      
+   }
+
+
+}
