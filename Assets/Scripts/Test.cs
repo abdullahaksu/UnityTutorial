94d457f3f6fb2453939b9bccfd8a4974d51068e5
abdullahaksu <175130227+abdullahaksu@users.noreply.github.com>
2025-02-15 00:03:20 +0300

    v0.07
    ders devam

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Data.Common;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class Test : MonoBehaviour
    int number = 5;
 
    int number2 = 6;
+
+   List<int> sayilar = new List<int>();
    void Start()
 
    {
@@ -24,13 +27,38 @@ public class Test : MonoBehaviour
 
 
       testFunction();
+
+      testDongulerFor();
+
+      testDongulerWhile();
+
+
+      sayilar.Add(10);
+      sayilar.Add(20);
+      sayilar.Add(30);
+      sayilar.Add(40);
+
+      foreach (int item in sayilar)
+      {
+         Debug.Log("Listedeki sayılardan biri = " + item);
+      }
+
+      sayilar.Remove(40);
+      sayilar.RemoveAt(2);
+
+      foreach (int item in sayilar)
+      {
+
+         Debug.Log("2 elemanı çıkardıktan sonra sayılar = " + item);
+
+      }
    }
 
    void Update()
 
    {
 
-      testDonguler();
+
 
 
    }
@@ -48,18 +76,46 @@ public class Test : MonoBehaviour
       }
    }
 
-   void testDonguler()
+   void testDongulerFor()
    {
+
+      //ilk başta int i = 0 diyerek değişken tanımladık istersek bu değişkeni yukarıda tanımlayıp buraya doğrudan tanımlanmış olan i değişkenini i=0; diye yazabilirdik
+      //İkinci bölümde değişkenin koşulunu sorguladık. i < 5 olduğu sürece döngü sürecek
+      //Koşul sürdüğü sürece her döngü sonunda i'yi 1 artırdık.
 
       for (int i = 0; i < 5; i++)
       {
          Debug.Log(i);
       }
 
-      foreach (var item in collection)
+
+   }
+
+   void testDongulerWhile()
+   {
+
+      //Yukarıda tanımlı number değişkeninin koşulunu bildirdik. Döngü her tamamlandığında 1 eksilttik. 
+
+
+      while (number > 0)
       {
-         
+         Debug.Log(number);
+         number--;
+         //number--; yerine number = number - 1 de yazabiliriz. Burada olaya matematik olarak değil atama olarak bakıyoruz. Number değişkeninin yeni değeri 1 eksiğine eşit olacak şekilde atama yaptık.
+
+
       }
+
+   }
+
+   void testArray()
+   {
+      //Bir dizi(array) tanımladık. İçeriğine 1den 5 e kadar sayıları yazdık
+      //Bu dizinin 3. indisi olan 3 değerine ulaşmak için 2 ile log aldık 
+      int[] numbers = { 1, 2, 3, 4, 5 };
+
+      Debug.Log(numbers[2]);
+
    }
 
 
